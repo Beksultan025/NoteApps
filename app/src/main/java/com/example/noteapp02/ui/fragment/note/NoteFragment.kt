6cbe7befa717f5f4a3284dd0d6ab28a0e366289c
Beksultan025 <ukuevb08@gmail.com>
2024-10-05 07:36:34 +0600

    Я сделал onBoard, при этом использовал TabLyout (indicator). Еще добавил всякие плавные анимации!

---
+++ app/src/main/java/com/example/noteapp02/ui/fragment/note/NoteFragment.kt
@@ -0,0 +1,35 @@
+package com.example.noteapp02.ui.fragment.note
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.noteapp02.databinding.FragmentNoteBinding
+
+class NoteFragment : Fragment() {
+
+    private lateinit var binding: FragmentNoteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNoteBinding.inflate(inflater,container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialize()
+        setupListeners()
+    }
+
+    private fun initialize() {
+
+    }
+
+    private fun setupListeners() {
+    }
+}
